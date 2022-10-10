3cd58bff2359a564e84da66c2eea5e03980ebd66
Rajat Kumar Singh <rajat7229@gmail.com>
2022-10-10 22:10:39 +0530

    completed home navbar and mobile foodtab

--- client/src/layouts/Homepage.layout.jsx
+++ client/src/layouts/Homepage.layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Navbar from '../components/Navbar';
-// import FoodTab from '../components/FoodTab';
+import Navbar from '../components/Navbar';
+import FoodTab from '../components/FoodTab';
 
 const HomepageLayout = (Components) => ({...props}) => {
   return (
     <>
-    {/* <Navbar />
-    <FoodTab /> */}
+    <Navbar />
+    <FoodTab />
     <div className='container mx-auto px-4 lg:px-20'>
         <Components {...props} />
     </div>
