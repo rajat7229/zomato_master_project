c3c54cb781db6d479335a27dcd5cec4122631850
Rajat Kumar Singh <rajat7229@gmail.com>
2022-10-10 23:27:39 +0530

    made delivery page

--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import HomepageLayout from '../layouts/Homepage.layout';
+import React from "react";
+import { useParams } from "react-router-dom";
+
+// Layout
+import HomeLayout from "../layouts/Homepage.layout";
+
+// components
+import Delivery from "../components/Delivery";
+import Dining from "../components/Dining";
+import NightLife from "../components/NightLife";
+import Nutrition from "../components/Nutrition";
 
 const Home = () => {
+  const { type } = useParams();
+
   return (
-    <div>Home</div>
+    <>
+      <div className="my-5 mb-20 md:mb-10">
+        {type === "delivery" && <Delivery />}
+        {type === "dining" && <Dining />}
+        {type === "night" && <NightLife />}
+        {type === "nutri" && <Nutrition />}
+      </div>
+    </>
   );
 };
 
-export default HomepageLayout(Home);
+export default HomeLayout(Home);
