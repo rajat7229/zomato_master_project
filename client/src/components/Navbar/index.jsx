55ee4a708d254817ba7de708cbeaf9a36da8393d
Rajat Kumar Singh <rajat7229@gmail.com>
2022-10-28 12:08:38 +0530

    created signin signup modal

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,49 +1,100 @@
-import React, {useState} from 'react';
-import {FaUserAlt} from 'react-icons/fa';
-import {HiLocationMarker} from 'react-icons/hi';
-import {IoMdArrowDropdown, IoMdArrowDropup} from 'react-icons/io';
-import {RiSearch2Line} from 'react-icons/ri';
+import React, { useState } from "react";
+import { FaUserAlt } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { RiSearch2Line } from "react-icons/ri";
 
-const MobileNav = ({user, isDropdownOpen, setIsDropdownOpen}) => {
+// components
+import SignUp from "../auth/SignUp";
+import SignIn from "../auth/SignIn";
+
+const MobileNav = ({
+  user,
+  isDropdownOpen,
+  setIsDropdownOpen,
+  signIn,
+  signUp,
+}) => {
+  const SignIn = () => {
+    signIn();
+    setIsDropdownOpen(false);
+  };
+
+  const SignUp = () => {
+    signUp();
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <div className='flex w-full items-center justify-between lg:hidden container m-auto py-2'>
-      <div className='w-28'>
-        <img src='https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png' alt='logo' className='w-full h-full' />
+    <div className="flex w-full items-center justify-between lg:hidden">
+      <div className="w-28">
+        <img
+          src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+          alt="logo"
+          className="w-full h-full"
+        />
       </div>
-      <div className='flex items-center gap-3 relative'>
-        <button className='bg-zomato-400 text-white py-2 px-3 rounded-full'>Use App</button>
+      <div className="flex items-center gap-3 relative">
+        <button className="bg-zomato-400 text-white py-2 px-3 rounded-full">
+          Use App
+        </button>
         {user?.fullName ? (
           <>
-          <div
-        onClick={() => setIsDropdownOpen((prev) => !prev)} 
-        className='border border-gray-300 text-zomato-400 w-9 h-9 rounded-full'>
-          <img src='https://cdn.xxl.thumbs.canstockphoto.com/young-man-avatar-cartoon-character-profile-picture-young-afroamerican-man-avatar-cartoon-character-image_csp70796176.jpg' alt='avatar' className='h-19 w-19 rounded-full object-cover'/>
-        </div>
-        {isDropdownOpen && (
-          <div className='absolute shadow-lg py-3 -bottom-20 -right-4 w-full z-20 flex flex-col gap-2 bg-white border border-gray-200'>
-          <button >Sign Out</button>
-        </div>
-        )}
+            <div
+              onClick={() => setIsDropdownOpen((prev) => !prev)}
+              className="border border-gray-300 text-zomato-400 w-9 h-9 rounded-full"
+            >
+              <img
+                src="https://cdn.xxl.thumbs.canstockphoto.com/young-man-avatar-cartoon-character-profile-picture-young-afroamerican-man-avatar-cartoon-character-image_csp70796176.jpg"
+                alt="avatar"
+                className="w-full h-full rounded-full object-cover"
+              />
+            </div>
+            {isDropdownOpen && (
+              <div className="absolute shadow-lg py-3 -bottom-14 w-36 z-20 flex flex-col gap-2 bg-white border border-gray-200">
+                <button>Sign Out</button>
+              </div>
+            )}
           </>
         ) : (
-        <>
-        <span onClick={() => setIsDropdownOpen((prev) => !prev)} className='border p-2 border-gray-400 rounded-full' >
-        <FaUserAlt className='w-full h-full' />
-      </span>
+          <>
+            <span
+              onClick={() => setIsDropdownOpen((prev) => !prev)}
+              className="border p-2 border-gray-400 rounded-full"
+            >
+              <FaUserAlt className="w-full h-full" />
+            </span>
 
-      {isDropdownOpen && (
-        <div className='absolute shadow-lg py-3 -bottom-20 -right-4 w-full z-20 flex flex-col gap-2 bg-white border border-gray-200'>
-        <button >Sign In</button>
-        <button> Sign Up</button>
-      </div>
-      )}
-      </>)}
+            {isDropdownOpen && (
+              <div className="absolute shadow-lg py-3 -bottom-24 w-full z-20 flex flex-col gap-2 bg-white border border-gray-200">
+                <button onClick={SignIn}>Sign In</button>
+                <button onClick={SignUp}>Sign Up</button>
+              </div>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
 };
 
-const LargeNav = ({user, setIsDropdownOpen, isDropdownOpen}) => {
+const LargeNav = ({
+  user,
+  isDropdownOpen,
+  setIsDropdownOpen,
+  signIn,
+  signUp,
+}) => {
+  const SignIn = () => {
+    signIn();
+    setIsDropdownOpen(false);
+  };
+
+  const SignUp = () => {
+    signUp();
+    setIsDropdownOpen(false);
+  };
+
   return (
     <div className="w-full items-center justify-between hidden lg:flex px-14">
       <div className="gap-4  items-center justify-around flex">
@@ -107,8 +158,8 @@ const LargeNav = ({user, setIsDropdownOpen, isDropdownOpen}) => {
 
             {isDropdownOpen && (
               <div className="absolute shadow-lg py-3 -bottom-24 -right-0 w-36 z-20 flex flex-col gap-2 bg-white border border-gray-200">
-                <button>Sign In</button>
-                <button>Sign Up</button>
+                <button onClick={SignIn}>Sign In</button>
+                <button onClick={SignUp}>Sign Up</button>
               </div>
             )}
           </>
@@ -119,21 +170,42 @@ const LargeNav = ({user, setIsDropdownOpen, isDropdownOpen}) => {
 };
 
 const Navbar = () => {
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [openSignUp, setOpenSignUp] = useState(false);
+
+  const openSignInModal = () => setOpenSignIn(true);
+  const openSignUpModal = () => setOpenSignUp(true);
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const user = {
-    fullName: "Rajat"
+    // fullName: "Rajat",
   };
 
   return (
-  <>
-  <nav className='p-4 lg:py-2 flex bg-white shadow-md lg:shadow-none lg:border-b-2 border-gray-100 w-full items-center'>
-    <MobileNav user={user} setIsDropdownOpen={setIsDropdownOpen} isDropdownOpen={isDropdownOpen} />
-    <LargeNav user={user} setIsDropdownOpen={setIsDropdownOpen} isDropdownOpen={isDropdownOpen} />
-  </nav>
-  </>
-  )
-}
+    <>
+      <SignIn isOpen={openSignIn} setIsOpen={setOpenSignIn} />
+      <SignUp isOpen={openSignUp} setIsOpen={setOpenSignUp} />
+
+      <nav className="p-4 lg:py-2 flex bg-white shadow-md lg:shadow-none lg:border-b-2 border-gray-100 w-full items-center">
+        <MobileNav
+          user={user}
+          setIsDropdownOpen={setIsDropdownOpen}
+          isDropdownOpen={isDropdownOpen}
+          signIn={openSignInModal}
+          signUp={openSignUpModal}
+        />
+
+        <LargeNav
+          user={user}
+          setIsDropdownOpen={setIsDropdownOpen}
+          isDropdownOpen={isDropdownOpen}
+          signIn={openSignInModal}
+          signUp={openSignUpModal}
+        />
+      </nav>
+    </>
+  );
+};
 
 export default Navbar;
